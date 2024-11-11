feacc9c8be28a914dc803db4b0d19b56fe3a95ff
@Prakul111 <iamprakul111@gmail.com>
2024-11-11 21:29:25 +0530

    another

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import "./user.css"
+import user from "./user.json"
+import { UserCard } from "./UserCard"
+
 export default function App() {
-  return "hi"
+  return <UserCard 
+  name={user.name} 
+  phoneNumber={user.phoneNumber}
+  age={user.age} 
+  address={user.address}
+  
+  />
 }
+
+
+
